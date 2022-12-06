a390201cf4e454272f39154e0bbb465c684ce310
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2022-12-06 18:42:36 +0200

    Delete install script

--- run.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-echo "Copying files to /srv/salt..."
-
-sudo mkdir -p /srv/salt/starter-module
-
-sudo cp init.sls /srv/salt/starter-module
-sudo cp -r micro/ /srv/salt/starter-module
-sudo cp -r apache2/ /srv/salt/starter-module
